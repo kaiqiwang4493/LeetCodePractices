8b97fa4a10f8eeb2a4b614bbd5be7da79f15be8f
Kaiqi Wang <kaiqiwang4493@gmail.com>
2019-10-08 23:46:17 -0400

    Check If Binary Tree Is Completed

--- src/parctices/BFS.java
+++ src/parctices/BFS.java
@@ -1,8 +1,12 @@
 package parctices;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
+import java.util.Queue;
+
+import dataStructure.TreeNode;
 
 public class BFS {
 	
@@ -28,5 +32,67 @@ public class BFS {
 		}
 		return result;
 	}
+	
+	
+	
+	/*
+	 * Check If Binary Tree Is Completed
+	 * 
+	 * Check if a given binary tree is completed. 
+	 * 
+	 * A complete binary tree is one in which every level of 
+	 * the binary tree is completely filled except possibly the last level.
+	 * 
+	 * Furthermore, all nodes are as far left as possible.
+	 * 
+	 * use a Queue to traverse the tree level by level.
+	 * if we find any node following a null node
+	 * we can say the tree is not a completed tree
+	 */
+	public boolean isCompleted(TreeNode root) {
+		if(root == null) {
+			return true;
+		}
+		
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		//false means does not meet any null node.
+		// true means has meet the null node.
+		boolean flag = false;
+		
+		while(!queue.isEmpty()) {
+			TreeNode temp = queue.poll();
+			if(temp.left != null) {
+				if(!flag) {
+					queue.add(temp.left);
+				}else {
+					return false;
+				}
+				
+			}else {
+				flag = true;
+			}
+			if(temp.right != null) {
+				if(!flag) {
+					queue.add(temp.right);
+				}else {
+					return false;
+				}
+			}else {
+				flag = true;
+			}
+		}
+		return true;
+	}
+	
+	/*
+	 * Kth Smallest Number In Sorted Matrix
+	 * 
+	 * Given a matrix of size N x M. 
+	 * For each row the elements are sorted in ascending order, 
+	 * and for each column the elements are also sorted in ascending order. 
+	 * Find the Kth smallest number in it.
+	 */
+	
 	
 }
